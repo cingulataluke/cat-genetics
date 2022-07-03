9c81b7527fdea84be82b97122601b0e7a00c2b1d
cingulataluke <43851672+cingulataluke@users.noreply.github.com>
2022-07-03 22:50:02 +0200

    add list of cats to interface

--- src/index.js
+++ src/index.js
@@ -6,20 +6,38 @@ const btnWildtypeCat = document.getElementById("wildtype-gen");
 
 const txtCurrentGenes = document.getElementById("current-genes");
 
+const divCards = document.getElementById("cards");
+let cardList = [];
+
 let currentCat;
 
-btnRandomCat.onclick = () =>
-{
-    currentCat = new Cat("Random", randomElem(["M", "F"]), Genes.randomGenes());
-    updateCurrentGenes();
+// TODO : refactor to stick to functional programming
+
+btnRandomCat.onclick = () => {
+	currentCat = new Cat("Random", randomElem(["M", "F"]), Genes.randomGenes());
+	newCatCard();
+	updateCurrentGenes();
 }
 
-btnWildtypeCat.onclick = () =>
-{
-    currentCat = new Cat("Wildtype", randomElem(["M", "F"]), Genes.wildtypeGenes());
-    updateCurrentGenes();
+btnWildtypeCat.onclick = () => {
+	currentCat = new Cat("Wildtype", randomElem(["M", "F"]), Genes.wildtypeGenes());
+	newCatCard();
+	updateCurrentGenes();
+}
+
+function newCatCard() {
+	let divCurrentCat = document.createElement("div");
+	divCurrentCat.setAttribute("class", "div-cat");
+	divCurrentCat.setAttribute("id", "div-cat-" + currentCat.id);
+	divCards.appendChild(divCurrentCat);
+	cardList.unshift(divCurrentCat);
 }
 
 function updateCurrentGenes() {
-    txtCurrentGenes.innerHTML = currentCat.showCat();
+	let divCurrentCat = cardList[0];
+	let txtCurrentCat = document.createElement("p");
+	txtCurrentCat.innerHTML = currentCat.showCat();
+	divCurrentCat.appendChild(txtCurrentCat);
+
+	txtCurrentGenes.innerHTML = currentCat.showCat();
 }
