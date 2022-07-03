cab3f4621ae0954f7a01b207021be88c0f1f3282
cingulataluke <43851672+cingulataluke@users.noreply.github.com>
2022-07-03 17:07:16 +0200

    upgrade random & show genes functions

--- src/genes.js
+++ src/genes.js
@@ -3,15 +3,16 @@ const allAlleles = [["B", "b", "b'"],["O", "o"],["D", "d"],["Dm", "dm"],
 					["TiA", "Ti+"],["I", "i"],["Wb", "wb"],["C", "cb", "cs", "c"],
 					["L", "l"], ["W", "w"]];
 
-function randomIndex(arr) {
-	// returns random int between 0 and arr.length-1
-	return Math.floor(Math.random()*arr.length);
+function randomElem(arr) {
+	// returns random element from an array
+	return arr[Math.floor(Math.random()*arr.length)];
 }
 
 class Genes {
 	// each gene is an array of 2 vals (for each chromosome)
 	// special case : orange gene is on x chromosome so only 1 copy in male cats
 	// not sure of the best way to treat that yet
+	// TODO : add genes
 	constructor(black, orange, dilute, dilute_modifier, 
 				white_spotting, agouti, tabby, spotted_tabby, 
 				ticked_tabby, silver, wide_band, colorpoint, 
@@ -34,11 +35,13 @@ class Genes {
 	}
 
 	showGenes() {
-		// TODO : rewrite with a loop
-		return this.black+" "+this.orange+" "+this.dilute+" "+this.dilute_modifier+" "+
-			   this.white_spotting+" "+this.agouti+" "+this.tabby+" "+this.spotted_tabby+" "+
-			   this.ticked_tabby+" "+this.silver+" "+this.wide_band+" "+this.colorpoint+" "+
-			   this.length+" "+this.dominant_white;
+		let string = "";
+		const keys = Object.keys(this);
+		for (let i in keys)
+		{
+			string += this[keys[i]][0] + this[keys[i]][1] + " ";
+		}
+		return string;
 	}
 
 	static randomGenes() {
@@ -46,7 +49,7 @@ class Genes {
 		let randomVals = [];
 		for (let i = 0 ; i < allAlleles.length ; i++)
 		{
-			randomVals.push([allAlleles[i][randomIndex(allAlleles[i])], allAlleles[i][randomIndex(allAlleles[i])]]);
+			randomVals.push([randomElem(allAlleles[i]), randomElem(allAlleles[i])]);
 		}
 		return new Genes(...randomVals);
 	}
@@ -65,4 +68,4 @@ class Genes {
 	}
 }
 
-export { Genes };
+export { Genes, randomElem };
